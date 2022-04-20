e03d6432606dec8ad2ca81438d88729d028be8e4
SantiagoSHF <102351257+SantiagoSHF@users.noreply.github.com>
2022-04-20 14:01:06 +0200

    Añadido primer ejercicio de arrays
    Este ejercicio de array te permite:
    - Escribir un array en HTML.
    - Usar un botón para escribir un ejemplo de array.
    - Sumar todos los valores del array escrito.
    - Calcular la media del array.
    - Te dice cual es el valor mayor y menor del array.

---
+++ ejercicio_Array1/ejercicio1.js
@@ -0,0 +1,69 @@
+function random(){
+    // Carga un array de ejemplo en el html cuando le das al botón Cargar Ejemplo
+    document.getElementById("text").value = "5,99,43,12,37";
+    }
+
+function aXa(){
+let a = document.getElementById("text").value.split(/,/);
+let s = 0;
+let md;
+let my = -Infinity;
+let m = +Infinity;
+let j = 0;
+
+// Suma cada valor del array
+for(i = 0; i < a.length; i++){
+    if(i == j)
+    {
+        s = parseInt(a[i]) + s;
+    }
+    j++;
+}
+
+// Calcula la md del array
+md = s / a.length;
+
+// Calcula el mayor
+for(i = 0; i < a.length; i++){
+    if(parseInt(a[i]) > my){
+        my = a[i];
+    }
+}
+
+// Calcula el menor
+for(i = 0; i < a.length; i++){
+    if(parseInt(a[i]) < m){
+        m = a[i];
+    }
+}
+
+// Pruebas antiguas
+//for(i = 0; i < a.length; i++){
+//    if(parseInt(a[i]) > wa){
+//        wa = a[i]
+//    }
+//}
+//for(i = 0; i < a.length; i++){
+//    if(parseInt(a[i]) == iwa){
+//        iwa = a[i]
+//    }
+//}
+//for(i = 0; i < a.length; i++){
+//    if(parseInt(a[i]) < m){
+//        m = a[i]
+//    }
+//}
+//for(i = 0; i < a.length; i++){
+//    if(parseInt(a[i]) <= menoiwa){
+//        menoiwa = a[i]
+//    }
+//}
+
+document.getElementById("resultado").innerHTML = "La suma es: " + s + "<br>"+
+"La media es: " + md + "<br>"+
+"El mayor es: " + my + "<br>"+
+"El menor es: " + m + "<br>";
+}
+function rpg(){
+    location.reload();
+}
