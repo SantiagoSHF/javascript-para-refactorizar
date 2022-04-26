21d738cc8f1ea9cd70df3897253fc57fb7ceab5a
SantiagoSHF <102351257+SantiagoSHF@users.noreply.github.com>
2022-04-26 19:10:52 +0200

    Update script_ejercicio_20.js

--- ejercicio_Basicos/ejercicio20/script_ejercicio_20.js
+++ ejercicio_Basicos/ejercicio20/script_ejercicio_20.js
@@ -1,25 +1,50 @@
-let iva, bi, peso, precio, envio;
+let i, bi, p, pr, e;
+// Introduce el BI del producto
 bi=parseFloat(prompt("Introduzca el BI del producto"));
-iva=prompt("Introduce el tipo de IVA (Normal o reducido)" );
-peso=parseFloat(prompt("Introduzca el peso del paquete en KG"));
+// Introduce el tipo de IVA del producto
+i=prompt("Introduce el tipo de IVA (Normal o reducido)" );
+// Introduce el peso del producto
+p=parseFloat(prompt("Introduzca el peso del paquete en KG"));
 
-if (peso < 1)
+// Precio de envio según el peso del producto
+if (p < 1)
 {
-    envio = 3;
+    e = 3;
 }
-else if(peso < 3)
+else 
 {
-    envio = 5;
-}
-else{
-    envio = 10;
-}
-
-if (iva == "normal" || iva == "Normal"){
-    precio = bi + (bi * 0.21) + envio;
-}
-else if(iva == "reducido" || iva == "Reducido"){
-    precio = bi + (bi * 0.10) + envio;
+    if(p < 3)
+    {
+        e = 5;
+    }
+    else
+    {
+        e = 10;
+    }
 }
 
-window.alert("El precio del producto es " + precio + "€");
+// Se selecciona el tipo de IVA
+if (i == "normal" || i == "Normal"){
+    pr = bi + (bi * 0.21) + e;
+}
+else
+{
+    if(i == "reducido" || i == "Reducido")
+    {
+        pr = bi + (bi * 0.10) + e;
+    }
+}
+
+//if (iva == "normal" || iva == "Normal"){
+//    pr = bi + (bi * 0.21) + envio;
+//}
+//else
+//{
+//    if(iva == "reducido" || iva == "Reducido")
+//    {
+//        pr = bi + (bi * 0.10) + envio;
+//    }
+//}
+
+// Se devuelve el precio del prodcuto
+window.alert("El precio del producto es " + pr + "€");
