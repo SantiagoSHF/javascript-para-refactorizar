13bc214efd257f7f8295cf647a25320f88c37fbf
SantiagoSHF <102351257+SantiagoSHF@users.noreply.github.com>
2022-04-29 06:52:48 +0200

    Revert "Se han añadido correcciones"

--- ejercicio_Basicos/ejercicio12/script_ejercicio_12.js
+++ ejercicio_Basicos/ejercicio12/script_ejercicio_12.js
@@ -1,37 +1,32 @@
-let days, month, year;
+let w, m, a;
 
 //Introducir día
-
-days = parseInt(prompt("Introduce un día"));
-while (days > 31 || days < 1)
+w=parseInt(prompt("Introduce un día"));
+while(w > 31 || w < 1)
 {
-    days = parseInt(prompt("Introduce un día correcto"));
+    w=parseInt(prompt("Introduce un día correcto"));
 }
 
 //Introducir mes
-
-month = parseInt(prompt("Introduce un mes"));
-while (month > 12 || month < 1)
+m=parseInt(prompt("Introduce un mes"));
+while(m > 12 || m < 1)
 {
-    month = parseInt(prompt("Introduce un mes correcto"));
+    m=parseInt(prompt("Introduce un mes correcto"));
 }
 
 //introducir año
-
-year = parseInt(prompt("Introduce un año"));
-while (year < 0)
+a=parseInt(prompt("Introduce un año"));
+while(a < 0)
 {
-    year = parseInt(prompt("Introduce un año correcto"));
+    a=parseInt(prompt("Introduce un año correcto"));
 
 }
 
 //Calcular si es navidad
+if(w == 25 && m == 12){
+    window.alert("La fecha introducida es " + w + "/" + m + "/" + a + " que se corresponde a NAVIDAD") 
+}
+else {
+    window.alert("su fecha introducida es " + w + "/" + m + "/" + a)
+}
 
-if (days == 25 && month == 12)
-{
-    window.alert("La fecha introducida es " + days + "/" + month + "/" + year + " que se corresponde a NAVIDAD") 
-}
-else 
-{
-    window.alert("su fecha introducida es " + days + "/" + month + "/" + year)
-}
